fd764d0beed31cb14e6f3c914793dbacf398977c
Ryoh Kawai <kawairyoh@gmail.com>
2020-07-04 01:14:37 +0900

    Add read file and parse pattern

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,11 @@ struct Cli {
 
 fn main() {
     let args = Cli::from_args();
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
 
-    println!("{:#?}", args);
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
 }
