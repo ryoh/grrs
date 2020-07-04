9bc9c17e755ddb171c1b5ede363860ca8c4f7d41
Ryoh Kawai <kawairyoh@gmail.com>
2020-07-04 16:49:08 +0900

    Change find_matches部分をモジュール化

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use exitfailure::ExitFailure;
 use failure::ResultExt;
-use std::io::{self, Write};
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -10,27 +9,12 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
-    for line in content.lines() {
-        if line.contains(pattern) {
-            writeln!(writer, "{}", line);
-        }
-    }
-}
-
 fn main() -> Result<(), ExitFailure> {
     let args = Cli::from_args();
     let content = std::fs::read_to_string(&args.path)
         .with_context(|_| format!("could not read file `{:?}`", &args.path))?;
 
-    find_matches(&content, &args.pattern, &mut std::io::stdout());
+    grrs::find_matches(&content, &args.pattern, &mut std::io::stdout());
 
     Ok(())
-}
-
-#[test]
-fn find_a_match() {
-    let mut result = Vec::new();
-    find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result);
-    assert_eq!(result, b"lorem ipsum\n");
 }
