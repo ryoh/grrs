3e59e8ea03181c2c97e6b8ab87fafa8e07f24815
Ryoh Kawai <kawairyoh@gmail.com>
2020-07-04 16:59:29 +0900

    Add ファイルが無いときの組合せテストを実装

--- tests/cli.rs
+++ tests/cli.rs
@@ -4,5 +4,12 @@ use std::process::Command;
 
 #[test]
 fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("grrs")?;
+
+    cmd.arg("foobar").arg("test/file/doent/exist");
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("No such file or directory"));
+
     Ok(())
 }
