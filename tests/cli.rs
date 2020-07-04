f82543941ca37990133f7ff74c0102c5119801f5
Ryoh Kawai <kawairyoh@gmail.com>
2020-07-04 17:10:51 +0900

    Add 成功時の総合テストを実装

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,6 +1,8 @@
 use assert_cmd::prelude::*;
 use predicates::prelude::*;
+use std::io::{self, Write};
 use std::process::Command;
+use tempfile::NamedTempFile;
 
 #[test]
 fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
@@ -10,6 +12,20 @@ fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
     cmd.assert()
         .failure()
         .stderr(predicate::str::contains("No such file or directory"));
+
+    Ok(())
+}
+
+#[test]
+fn find_content_in_file() -> Result<(), Box<dyn std::error::Error>> {
+    let mut file = NamedTempFile::new()?;
+    writeln!(file, "A test\nActual content\nMore content\nAnother test")?;
+
+    let mut cmd = Command::cargo_bin("grrs")?;
+    cmd.arg("test").arg(file.path());
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("test\nAnother test"));
 
     Ok(())
 }
