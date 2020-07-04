57602ffd8a9f77ac486ca7a0141a23125e91275e
Ryoh Kawai <kawairyoh@gmail.com>
2020-07-04 13:26:55 +0900

    Change printing for performance up.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use exitfailure::ExitFailure;
 use failure::ResultExt;
+use std::io::{self, Write};
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -10,12 +11,15 @@ struct Cli {
 }
 
 fn main() -> Result<(), ExitFailure> {
-    //let args = Cli::from_args();
-    let path = "test.txt";
+    let args = Cli::from_args();
+    let path = &args.path;
     let content = std::fs::read_to_string(path)
-        .with_context(|_| format!("could not read file `{}`", path))?;
+        .with_context(|_| format!("could not read file `{:?}`", path))?;
 
-    println!("file content: {}", content);
+    let stdout = io::stdout();
+    let mut handle = io::BufWriter::new(stdout);
+
+    writeln!(handle, "content: {}", content);
 
     /*
     for line in content.lines() {
