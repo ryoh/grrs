05f6031fabedc05e9e3c8092fcbda07e2cc3f623
Ryoh Kawai <kawairyoh@gmail.com>
2020-07-04 13:33:23 +0900

    Change unit testのためにリファクタ

--- src/main.rs
+++ src/main.rs
@@ -10,20 +10,21 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
+fn find_matches(content: &str, pattern: &str) {
+    for line in content.lines() {
+        if line.contains(pattern) {
+            println!("{}", line);
+        }
+    }
+}
+
 fn main() -> Result<(), ExitFailure> {
     let args = Cli::from_args();
     let path = &args.path;
     let content = std::fs::read_to_string(path)
         .with_context(|_| format!("could not read file `{:?}`", path))?;
 
-    let stdout = io::stdout();
-    let mut handle = io::BufWriter::new(stdout);
-
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            writeln!(handle, "{}", line);
-        }
-    }
+    find_matches(&content, &args.pattern);
 
     Ok(())
 }
